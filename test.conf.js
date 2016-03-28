34e25910561a9685185306c18e5bea927f04380f
zikkurat <godwww@gmail.com>
2016-03-28 19:35:10 +0800

    test config

--- test.conf.js
+++ test.conf.js
@@ -5,59 +5,123 @@
 	<![CDATA[
 		// 载入插件 {{{
 		var gulp = require('gulp'),
-			less = require('gulp-less'),//less编译
-			min_css = require('gulp-minify-css'),//压缩css
+			//less = require('gulp-less'),//less编译
+			//min_css = require('gulp-minify-css'),//压缩css
 			uglify = require('gulp-uglify'),//转换、压缩JS
 			concat = require('gulp-concat'),//合并文件
-			rename = require('gulp-rename'),//重命名
-			html_min = require('gulp-htmlmin'),//压缩html
-			plumber = require('gulp-plumber');//防止watch出错退出
-			sourcemaps = require('gulp-sourcemaps');//源映射
-			livereload = require('gulp-livereload');//刷新浏览器
+			//rename = require('gulp-rename'),//重命名
+			//html_min = require('gulp-htmlmin'),//压缩html
+			plumber = require('gulp-plumber'),//防止watch出错退出
+			//sourcemaps = require('gulp-sourcemaps'),//源映射
+			livereload = require('gulp-livereload'),//刷新浏览器
+			amd = require('amd-optimize'),
+			react = require('gulp-react');
 		//}}}
 
-		var xxx = 0;
+		//var xxx = 0;
+
 		// less {{{
-		gulp.task('less', function() {
-			gulp.src('../test/source/main.less')
-				.pipe(changed('../test/', {
-				    extension: '.html'
-				}))
-				.pipe(plumber())
-				.pipe(less())
-				.pipe(min_css({compatibility:'ie7'}))
-				.pipe(rename(function(path) {
-					xxx++;
-					console.log('---------   '+xxx+'   ---------');
-					console.log(path.basename + path.extname);
-					path.basename += '.min'
-				}))
-				.pipe(gulp.dest('../test/'));
-				//.pipe(livereload({auto:false}));
-		});
+		//gulp.task('less', function() {
+			//gulp.src('../test/source/main.less')
+				//.pipe(changed('../test/', {
+					//extension: '.html'
+				//}))
+				//.pipe(plumber())
+				//.pipe(less())
+				//.pipe(min_css({compatibility:'ie7'}))
+				//.pipe(rename(function(path) {
+					//xxx++;
+					//console.log('---------   '+xxx+'   ---------');
+					//console.log(path.basename + path.extname);
+					//path.basename += '.min'
+				//}))
+				//.pipe(gulp.dest('../test/'));
+				////.pipe(livereload({auto:false}));
+		//});
 		//}}}
 
 		// uglify {{{
-		gulp.task('uglify', function() {
-		    gulp.src('../test/source/*.js')
-				.pipe(plumber())
-				.pipe(sourcemaps.init())
-		        .pipe(uglify({
-		            preserveComments: 'some'
-		        }))
-		        .pipe(rename(function(path) {
-					path.basename += '.min';
-		        }))
-				//.pipe(sourcemaps.write(paths.js.map))
-				.pipe(gulp.dest('../test/'))
-		});
+		//gulp.task('uglify', function() {
+			//gulp.src('../test/source/*.js')
+				//.pipe(plumber())
+				//.pipe(sourcemaps.init())
+				//.pipe(uglify({
+					//preserveComments: 'some'
+				//}))
+				//.pipe(rename(function(path) {
+					//path.basename += '.min';
+				//}))
+				////.pipe(sourcemaps.write(paths.js.map))
+				//.pipe(gulp.dest('../test/'))
+		//});
 		//}}}
+
+		gulp.task('html', function() {
+			return gulp.src('../nikksy/test/*.html')
+						.pipe(plumber())
+						.pipe(gulp.dest('../nikksy/'))
+						.pipe(livereload({auto:false}));
+		});
+
+		gulp.task('js', function() {
+			return gulp.src('')
+						.pipe(livereload({auto:false}));
+		});
+
+		gulp.task('ui',function() {
+			gulp.src('../nikksy/test/jsx/*.jsx')
+				.pipe(react())
+				//.pipe(concat('ui.js'))
+				.pipe(gulp.dest('../nikksy/test/jsx'));
+
+			return gulp.src('../nikksy/test/jsx/*.js')
+						.pipe(amd('ui',{
+							baseUrl: '../nikksy/test/jsx/',
+							paths:{
+								jquery:'../../js/jquery-2.1.4.min',
+								react:'../../js/react-with-addons.min',
+								ReactDom:'../../js/react-dom.min',
+							},
+							exclude:['jquery','react','ReactDom'],
+						}))
+						.pipe(concat('ui.js'))
+						.pipe(gulp.dest('../nikksy/test_dest'));
+		});
+
+		//gulp.task('amdmake', function() {
+			//gulp.src('../nikksy/test/js/main.jsx')
+				//.pipe(react())
+				//.pipe(gulp.dest('../nikksy/test/js'));
+
+			//return gulp.src('../nikksy/test/mod/*.js')
+				//.pipe(plumber())
+				//.pipe(amd('../js/main', {
+					//baseUrl: '../nikksy/test/mod/',
+					//exclude: ['jquery', 'react', 'ReactDom','btn'],
+					//paths: {
+						//'jquery': '../../js/jquery-2.1.4.min',
+						//'react': '../../js/react-with-addons.min',
+						//'ReactDom': '../../js/react-dom.min',
+						//'btn':'../jsx/button'
+					//}
+				//}))
+				//.pipe(concat('main.js'))
+				//.pipe(uglify())
+				//.pipe(gulp.dest('../nikksy/test_dest'));
+		//});
+
+		gulp.task('r',function() {
+		});
 
 		// wathc{{{
 		gulp.task('watch', function() {
-			//livereload.listen();
-			gulp.watch('../test/source/*.less', ['less']);
-			gulp.watch('../test/source/*.js', ['uglify']);
+			gulp.watch('../nikksy/test/*.html',['html']);
+			gulp.watch('../nikksy/test/jsx/*.jsx',['ui','js']);
+			//gulp.watch(['../nikksy/test/**/*.js', '../nikksy/test/**/*.jsx'], ['js', 'amdmake']);
+			//gulp.watch(['../nikksy/test/**/*.jsx'],['js','r']);
+			livereload.listen();
+			//gulp.watch('../test/source/*.less', ['less']);
+			//gulp.watch('../test/source/*.js', ['uglify']);
 		});
 		//}}}
 
